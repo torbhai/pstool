91b847629f8549b8fc8b2d73f22ed51209653066
torbhai <70181915+torbhai@users.noreply.github.com>
2020-08-30 19:40:53 +0530

    Create test32.py

---
+++ test32.py
@@ -0,0 +1,13 @@
+#!/user/bin/python
+print ('''created and developed by tor bhai studio. visit us on our site to learn hacking , programming, web developement and many more for free.
+https://torbhai.in ''')
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = input("[*] enter the host/ip to scan: ")
+port = input(int("[*] enter the port to scan: "))
+def portscanner(port):
+	if sock.connect_ex((host,port)):
+		print ('port is closed')
+	else:
+		print ('given ip is open on the port')
+portscanner(port)		
